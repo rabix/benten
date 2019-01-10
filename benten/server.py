813591eda81b4d2571e3d38fd41305fd64b0e310
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2019-01-10 16:38:49 -0500

    Commenting out unimplemented functions
    Prelude to moving lang server infrastructure to own directory

--- benten/server.py
+++ benten/server.py
@@ -43,6 +43,8 @@ class LangServer:
         self.all_symbols = None
         self.workspace = None
         self.streaming = True
+
+        self.open_documents = {}
         self.initialization_request_received = False
         logger.info("Starting up ...")
 
@@ -82,16 +84,16 @@ class LangServer:
         try:
             response = {
                 "initialize": self.serve_initialize,
-                "textDocument/didOpen": self.serve_doc_open,
+                "textDocument/didOpen": self.serve_doc_did_open,
                 "textDocument/hover": self.serve_hover,
-                "textDocument/definition": self.serve_definition,
-                "textDocument/xdefinition": self.serve_x_definition,
-                "textDocument/references": self.serve_references,
-                "workspace/xreferences": self.serve_x_references,
-                "textDocument/documentSymbol": self.serve_document_symbols,
-                "workspace/symbol": self.serve_symbols,
-                "workspace/xpackages": self.serve_x_packages,
-                "workspace/xdependencies": self.serve_x_dependencies,
+                # "textDocument/definition": self.serve_definition,
+                # "textDocument/xdefinition": self.serve_x_definition,
+                # "textDocument/references": self.serve_references,
+                # "workspace/xreferences": self.serve_x_references,
+                # "textDocument/documentSymbol": self.serve_document_symbols,
+                # "workspace/symbol": self.serve_symbols,
+                # "workspace/xpackages": self.serve_x_packages,
+                # "workspace/xdependencies": self.serve_x_dependencies,
                 "$/cancelRequest": self.serve_ignore,
                 "shutdown": self.serve_ignore,
                 "exit": self.serve_exit,
@@ -169,5 +171,19 @@ class LangServer:
     def serve_exit(self, client_query):
         self.running = False
 
-    def serve_doc_open(self, client_query):
+    # https://microsoft.github.io/language-server-protocol/specification#textDocument_didOpen
+    # The document open notification is sent from the client to the server to signal newly opened text documents. The
+    # document’s truth is now managed by the client and the server must not try to read the document’s truth using
+    # the document’s Uri. Open in this sense means it is managed by the client. It doesn’t necessarily mean that its
+    # content is presented in an editor. An open notification must not be sent more than once without a corresponding
+    # close notification send before. This means open and close notification must be balanced and the max open count
+    # for a particular textDocument is one. Note that a server’s ability to fulfill requests is independent of
+    # whether a text document is open or closed.
+
+    # Notes:
+    # Multiple documents can be open at the same time. We need to keep track of this.
+    def serve_doc_did_open(self, client_query):
         pass
+
+    def serve_hover(self, client_query):
+        return {""}
