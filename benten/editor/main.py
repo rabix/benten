01964944be8c0d9b9de2cf6e40a07a1a0896ee75
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2019-02-04 14:43:31 -0500

    The basic breadcrumb idea works
    1. However, for proper encapsulation we should
       subclass QTabWidget and call that, say,
       BentenMainWidget and extract it into it's own file.
    
    2. In order to synchronize edits of nested inline steps
       we need to enhance the CwlDoc so we can share it
       between BentenWindows and tell a BentenWindow to
       operate on a section of it.

--- benten/editor/main.py
+++ benten/editor/main.py
@@ -5,7 +5,8 @@ import pathlib
 
 from PySide2.QtCore import Qt, QDateTime, QModelIndex, QSignalBlocker, QRect, Qt, QTimeZone, Slot
 
-from PySide2.QtWidgets import QAction, QApplication, QPushButton, QLabel, QHBoxLayout, QVBoxLayout, QHeaderView, \
+from PySide2.QtWidgets import QAction, QApplication, QTabWidget, QPushButton, QLabel, QHBoxLayout, QVBoxLayout, \
+    QHeaderView, \
     QMenuBar, QMainWindow, QLineEdit, QSizePolicy, QTableView, QWidget
 
 from benten.editor.bentenwindow import BentenWindow
@@ -35,8 +36,18 @@ class MainWindow(QMainWindow):
         # geometry = app.desktop().availableGeometry(self)
         # self.setFixedSize(geometry.width() * 0.8, geometry.height() * 0.7)
 
-        self.benten_window = BentenWindow()
-        self.setCentralWidget(self.benten_window)
+        self.tab_widget = QTabWidget()
+        # https://stackoverflow.com/questions/18022290/qt5-align-osx-qtabwidget-left
+        self.tab_widget.setStyleSheet("QTabWidget::tab-bar {left: 0; }")
+        self.tab_widget.currentChanged.connect(self.breadcrumb_selected)
+
+        self.tab_widget.addTab(BentenWindow(), "Benten")
+
+        self.tab_widget.addTab(BentenWindow(), "Remove this benten")
+
+        self.setCentralWidget(self.tab_widget)
+
+        self.benten_window = self.tab_widget.currentWidget()
         self.benten_window.code_editor.setFocus()
 
         if path_str is not None:
@@ -52,6 +63,19 @@ class MainWindow(QMainWindow):
     @Slot()
     def exit_app(self, checked):
         sys.exit()
+
+    def get_current_view(self):
+        pass
+
+    def open_sub_document(self):
+        pass
+
+    @Slot()
+    def breadcrumb_selected(self):
+        # Reload from disk
+        # If no differences, nothing to do
+        # If different, apply a squashed edit to bring us up to the latest version
+        print(self.tab_widget.currentIndex())
 
 
 def main():
