52df1ee3cc3885a0334e8e7d48822e3eaec5f99c
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2019-02-07 07:58:51 -0500

    Implement getting nested step

--- benten/editing/cwldoc.py
+++ benten/editing/cwldoc.py
@@ -46,15 +46,22 @@ class DocEdit:
         self.edit_type = edit_type
 
 
-class CwlDoc:
-    def __init__(self, fname: pathlib.Path=None, raw_cwl: str=None):
-        if fname is not None:
-            self.raw_cwl = fname.open("r").read()
-        elif raw_cwl is not None:
-            self.raw_cwl = raw_cwl
-        else:
-            raise ValueError("Need to pass path to CWL or raw CWL string")
+class InlineChild:
+    # Note that start_line and end_line can change
+    def __init__(self, parent: "CwlDoc", start_line: int, end_line: int):
+        self.parent = parent
+        self.start_line = start_line
+        self.end_line = end_line
 
+    def apply_child_edit(self):
+        pass
+
+
+class CwlDoc:
+    def __init__(self, raw_cwl: str, path: pathlib.Path, inline_path: str=None):
+        self.raw_cwl = raw_cwl
+        self.path = path
+        self.inline_path = inline_path
         self.cwl_lines = self.raw_cwl.splitlines()
         self.cwl_dict = yaml.load(self.raw_cwl, Loader=CLineLoader)
 
@@ -79,3 +86,31 @@ class CwlDoc:
 
         self.raw_cwl = "\n".join(self.cwl_lines)
         self.cwl_dict = yaml.load(self.raw_cwl, Loader=CLineLoader)
+
+    # No error checking here because this will be asked for programatically only
+    # if the nested dict exists
+    def get_nested_inline_step(self, inline_path: [str]):
+
+        def _find_step(_doc_dict, _inline_path):
+            if len(_inline_path) == 0:
+                return _doc_dict
+            else:
+                for steps in _doc_dict["steps"]:
+                    if steps["id"] == _inline_path[0]:
+                        return _find_step(steps["run"], _inline_path[1:])
+
+        if self.inline_path is not None:
+            raise RuntimeError("Sub part from nested document fragment is not allowed")
+
+        nested_doc = _find_step(self.cwl_dict, inline_path)
+
+        # Now we have a dict with line numbers, which we'll use to extract from the main doc
+        meta = nested_doc["meta"]
+        lines_we_need = self.cwl_lines[meta["__start_line__"]:meta["__end_line__"]]
+        indent_level = len(lines_we_need[0]) - len(lines_we_need[0].lstrip())
+        lines = [
+            l[indent_level:]
+            for l in lines_we_need
+        ]
+
+        return CwlDoc(raw_cwl="\n".join(lines), path=self.path, inline_path=inline_path)
