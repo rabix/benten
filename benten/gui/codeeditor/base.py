8e10e8c4dad374e47ec2ec21aaaddbd5975f61ae
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2019-02-12 17:10:44 -0500

    Improve text insertion
    1. Use select all + insert technique to preserve
       history
    2. Do better at preserving position state
    
    ToDo:
    Keep track of horizontal cursor too, as needed.

--- benten/gui/codeeditor/base.py
+++ benten/gui/codeeditor/base.py
@@ -604,12 +604,22 @@ class CodeEditorBase(QtWidgets.QPlainTextEdit):
     def set_text(self, text, preserve_state=True):
         # This attempts to preserve the editor state before and after the insert
         # from https://www.qtcentre.org/threads/13933-Restoring-cursor-position-in-QTextEdit
+
+        scroll_value = self.verticalScrollBar().value()
         original_cursor_pos = self.textCursor().position()
-        self.setPlainText(text)
+
+        # https://programtalk.com/python-examples/PySide2.QtGui.QTextCursor/
+        # https://www.qtcentre.org/threads/43268-Setting-Text-in-QPlainTextEdit-without-Clearing-Undo-Redo-History
+        doc = self.document()
+        insert_cursor = QtGui.QTextCursor(doc)
+        insert_cursor.select(QtGui.QTextCursor.SelectionType.Document)
+        insert_cursor.insertText(text)
+        self.updateMargins()
+
+        self.verticalScrollBar().setValue(scroll_value)
         new_cursor = self.textCursor()
         new_cursor.setPosition(original_cursor_pos, QtGui.QTextCursor.MoveAnchor)
         self.setTextCursor(new_cursor)
-        self.updateMargins()
 
     ## Some basic options
     
