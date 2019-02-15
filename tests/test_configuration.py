296a6a827c41e32619702c0ffbbbc5cb07a6b677
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2019-02-14 22:10:46 -0500

    Move relevant code into setup/teardown

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -5,15 +5,24 @@ import pathlib
 from benten.configuration import Configuration
 
 
+test_dir = "./benten-test-config"
+
+
+def setup():
+    # First we make sure there is no existing config file, say from a previous run
+    shutil.rmtree(pathlib.Path(test_dir), ignore_errors=True)
+
+
+def teardown():
+    shutil.rmtree(pathlib.Path(test_dir), ignore_errors=True)
+
+
 # monkey patch is amazing!
 # https://docs.pytest.org/en/latest/monkeypatch.html
 # https://holgerkrekel.net/2009/03/03/monkeypatching-in-unit-tests-done-right/
 def test_basic(monkeypatch):
-    monkeypatch.setitem(os.environ, "XDG_CONFIG_HOME", "./benten-test-config")
-    monkeypatch.setitem(os.environ, "XDG_DATA_HOME", "./benten-test-config")
-
-    # First we make sure there is no existing config file, say from a previous run
-    shutil.rmtree(pathlib.Path("./benten-test-config"), ignore_errors=True)
+    monkeypatch.setitem(os.environ, "XDG_CONFIG_HOME", test_dir)
+    monkeypatch.setitem(os.environ, "XDG_DATA_HOME", test_dir)
 
     # test creation of config dirs and default file
     config = Configuration()
