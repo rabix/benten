9cc99fc09623004e45697d1cc8691b89d072fa2e
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2019-02-17 08:12:59 -0500

    Scene will now match view size

--- benten/gui/processview.py
+++ benten/gui/processview.py
@@ -1,7 +1,14 @@
 from PySide2.QtWidgets import QGraphicsView
+from PySide2.QtGui import QResizeEvent
+from PySide2.QtCore import Qt
 
 
 class ProcessView(QGraphicsView):
     def __init__(self, parent):
         super().__init__(parent)
         self.setAcceptDrops(True)
+
+    def resizeEvent(self, event:QResizeEvent):
+        super().resizeEvent(event)
+        if self.scene():
+            self.fitInView(self.scene().sceneRect(), Qt.KeepAspectRatio)
