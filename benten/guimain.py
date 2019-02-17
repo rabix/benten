f675861c1717e2c23ec1e0f5e6f4015b8769180c
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2019-02-17 12:47:21 -0500

    Fix: jumping window bug
    Implement better way of setting initial geometry
    Use QSettings to store geometry.
    
    Todo: Add commandline option to reset geometry in case
    QT screws up, say because of multi-monitors etc etc

--- benten/guimain.py
+++ benten/guimain.py
@@ -2,8 +2,8 @@ import argparse
 import sys
 import pathlib
 
-from PySide2.QtCore import Slot
-
+from PySide2.QtCore import QSettings, Slot
+from PySide2.QtGui import QCloseEvent
 from PySide2.QtWidgets import QAction, QActionGroup, QApplication, QMainWindow
 
 from .configuration import Configuration
@@ -19,7 +19,11 @@ class MainWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
 
-        self.setWindowTitle("Benten")
+        self.resize(800, 600)  # default size
+        settings = QSettings("sbg", "benten")
+        geometry = settings.value("geometry")
+        if geometry is not None:
+            self.restoreGeometry(geometry)
 
         # This needs to come before tabs are added because adding tabs triggers
         # the currentChanged signal which triggers a slot that requires this to
@@ -53,15 +57,14 @@ class MainWindow(QMainWindow):
         exit_action.triggered.connect(self.exit_app)
         file_menu.addAction(exit_action)
 
-        if sbg_profiles.profiles:
-            self._add_profile_menu(menu, sbg_profiles.profiles)
-
-        self._add_cwl_menu(menu)
+        self._add_profile_menu(menu, sbg_profiles.profiles)
+        self._add_cwl_menu(menu, sbg_profiles.profiles)
 
         # menu.setNativeMenuBar(False) # We actually don't want this, native mac is ... nicer
 
-
     def _add_profile_menu(self, menu, profile_list):
+        if len(profile_list) == 0: return
+
         prof_menu = menu.addMenu("SBG &profile")
         prof_action_group = QActionGroup(prof_menu)
         for prof_name in profile_list:
@@ -72,28 +75,32 @@ class MainWindow(QMainWindow):
             prof_menu.addAction(profile_action)  # Apparently we still need to add these
         prof_action_group.actions()[0].setChecked(True)
 
-    def _add_cwl_menu(self, menu):
+    def _add_cwl_menu(self, menu, profile_list):
         cwl_menu = menu.addMenu("File")
 
         save_action = QAction("&Save", self)
         save_action.setShortcut("Ctrl+S")
-        #save_action.triggered.connect(...)
+        save_action.triggered.connect(self.tab_widget.cwl_save)
         cwl_menu.addAction(save_action)
 
-        push_action = QAction("&Push", self)
-        push_action.setShortcut("Ctrl+P")
-        #save_action.triggered.connect(...)
-        cwl_menu.addAction(push_action)
+        if profile_list:
+            push_action = QAction("&Push", self)
+            push_action.setShortcut("Ctrl+P")
+            #save_action.triggered.connect(...)
+            cwl_menu.addAction(push_action)
+
+    def closeEvent(self, event: QCloseEvent):
+        settings = QSettings("sbg", "benten")
+        settings.setValue("geometry", self.saveGeometry())
+        super().closeEvent(event)
 
     @Slot()
     def exit_app(self, checked):
         sys.exit()
 
-    def get_current_view(self):
-        pass
-
-    def open_sub_document(self):
-        pass
+    @Slot(str)
+    def set_title(self, title):
+        self.setWindowTitle(title)
 
     @Slot()
     def breadcrumb_selected(self):
@@ -124,8 +131,9 @@ def main():
     window.show()
 
     # Window dimensions
-    geometry = app.desktop().availableGeometry()
-    window.setBaseSize(geometry.width() * 0.8, geometry.height() * 0.7)
+    # on macOS this causes the jumping window bug: https://bugreports.qt.io/browse/PYSIDE-941
+    # geometry = app.desktop().availableGeometry()
+    # window.setBaseSize(geometry.width() * 0.8, geometry.height() * 0.7)
 
     # Load the file AFTER the geometry is set ...
     path_str = args.cwl
