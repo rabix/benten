d07c675a72897bfe3a2c8f23c402552ae9bc2ac3
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2019-02-19 21:43:23 -0500

    Implement JsonMixin to convert JSON to CWL YAML
    Also offers option to strip SBG tags from JSON

---
+++ benten/sbg/jsonmixin.py
@@ -0,0 +1,41 @@
+"""Load a JSON file, strip out insessential SBG tags and convert it into YAML. Change the
+base file name if needed."""
+from ..editing.listormap import yaml, Loader
+
+
+def _strip_sbg_tags(node):
+    if isinstance(node, dict):
+        return {
+            k: _strip_sbg_tags(v)
+            for k, v in node.items()
+            if k[:4] != "sbg:"
+        }
+    elif isinstance(node, list):
+        return [
+            _strip_sbg_tags(n)
+            for n in node
+        ]
+    else:
+        return node
+
+
+def import_json(raw_json, strip_sbg_tags=True):
+    d = yaml.load(raw_json, Loader=Loader)
+    if strip_sbg_tags:
+        d = _strip_sbg_tags(d)
+    return d
+
+
+class JsonMixin:
+    def __init__(self, *args, raw_json=None, strip_sbg_tags=True, new_path=None, **kwargs):
+        # This mixin is special. It should be the first mixin because it's kind of a bouncer,
+        # tossing out JSON and replacing it with YAML. In any case, it wants to do its processing
+        # first, before letting other classes have a go. It does all it does in the initializer
+        # new_path has to be supplied.
+        if raw_json is not None:
+            if new_path is None:
+                raise RuntimeError("New CWL filename after conversion must be provided")
+            kwargs["raw_cwl"] = yaml.safe_dump(import_json(raw_json, strip_sbg_tags=strip_sbg_tags))
+            kwargs["path"] = new_path
+
+        super(JsonMixin, self).__init__(*args, **kwargs)
---
+++ tests/test_jsonmixin.py
@@ -0,0 +1,63 @@
+import pathlib
+import os
+import shutil
+import pytest
+
+from benten.sbg.jsonmixin import JsonMixin
+from benten.editing.cwldoc import CwlDoc
+
+
+current_path = pathlib.Path(__file__).parent
+test_dir = "jsonmixin-test-temp-cwl-dir"
+
+
+def setup():
+    if not os.path.exists(test_dir):
+        os.mkdir(test_dir)
+
+    shutil.copy(pathlib.Path(current_path, "cwl", "002.nested.inline.sbg.eco", "wf3.json"), test_dir)
+    shutil.copy(pathlib.Path(current_path, "cwl", "002.nested.inline.sbg.eco", "wf3.cwl"), test_dir)
+
+
+def teardown():
+    if os.path.exists(test_dir):
+        shutil.rmtree(test_dir)
+
+
+def test_json_basic():
+
+    class SBGCwlDoc(JsonMixin, CwlDoc):
+        def __init__(self, *args, **kwargs):
+            super(SBGCwlDoc, self).__init__(*args, **kwargs)
+
+    # Existing behavior should not change
+    wf_path = pathlib.Path(test_dir, "wf3.cwl")
+    c = SBGCwlDoc(raw_cwl=wf_path.open("r").read(), path=wf_path, inline_path=None)
+    c.compute_cwl_dict()
+
+    assert c.cwl_dict["label"] == "wf3"
+    assert len(c.cwl_dict["steps"]) == 3
+
+    # Now test new behavior with json file
+    wf_path = pathlib.Path(test_dir, "wf3.json")
+    new_wf_path = pathlib.Path(test_dir, "wf3.cwl")
+
+    with pytest.raises(RuntimeError):
+        _ = SBGCwlDoc(raw_json=wf_path.open("r").read())
+
+    c = SBGCwlDoc(raw_json=wf_path.open("r").read(), new_path=new_wf_path, inline_path=None)
+    c.compute_cwl_dict()
+
+    assert c.cwl_dict["label"] == "wf3"
+    assert len(c.cwl_dict["steps"]) == 3
+
+    # SBG tags should be stripped by default
+    assert not any(k for k, _ in c.cwl_dict if k[:4] == "sbg:")
+    assert not any(k for k, _ in c.cwl_dict["steps"]["wf0"] if k[:4] == "sbg:")
+
+    # SBG tags should be kept
+    c = SBGCwlDoc(raw_json=wf_path.open("r").read(), new_path=new_wf_path, strip_sbg_tags=False)
+    c.compute_cwl_dict()
+
+    assert any(k for k, _ in c.cwl_dict if k[:4] == "sbg:")
+    assert any(k for k, _ in c.cwl_dict["steps"]["wf0"] if k[:4] == "sbg:")
