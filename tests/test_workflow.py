a94a85c5ad7d21fbc9df94361d2216cfd8c3074c
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2019-02-27 20:00:49 -0500

    Add tests for step add code

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -218,6 +218,39 @@ def test_connection_search():
     assert wf.find_connection(c3) is None
 
 
+def test_add_to_doc_missing_step_field():
+    wf_with_no_step = """class: Workflow"""
+    cwl_doc = WF.CwlDoc(raw_cwl=wf_with_no_step, path=pathlib.Path(current_path, "./nothing.cwl").resolve())
+    cwl_doc.compute_cwl_dict()
+    wf = WF.Workflow(cwl_doc=cwl_doc)
+
+    edit = wf.add_step(pathlib.Path(current_path, "cwl/001.basic/arguments.cwl"))
+
+    assert edit.start.line == 1
+    assert edit.start.column == 0
+    assert edit.end is None
+
+    lines = edit.text.splitlines()
+    assert lines[1].lstrip().startswith("arguments.cwl:")
+
+
+def test_add_to_doc_empty_step_field():
+    wf_with_empty_step = """class: Workflow\nsteps: []"""
+    cwl_doc = WF.CwlDoc(raw_cwl=wf_with_empty_step, path=pathlib.Path(current_path, "./nothing.cwl").resolve())
+    cwl_doc.compute_cwl_dict()
+    wf = WF.Workflow(cwl_doc=cwl_doc)
+
+    edit = wf.add_step(pathlib.Path(current_path, "cwl/001.basic/arguments.cwl"))
+
+    assert edit.start.line == 1
+    assert edit.start.column == 0
+    assert edit.end.line == 1
+    assert edit.end.column == 9
+
+    lines = edit.text.splitlines()
+    assert lines[1].lstrip().startswith("arguments.cwl:")
+
+
 def test_add_local_step_list():
 
     wf_path = pathlib.Path(current_path, "cwl/001.basic/wf-steps-as-list.cwl").resolve()
