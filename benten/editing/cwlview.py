4fc4c43513fef24b4aaad24183c80db8bea0be38
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2019-03-02 07:23:02 -0500

    wip: Saving state
    A CwlView is a persistent object (whereas CwlDoc was
    disposable). It takes over the functions of
    DocumentManager. So I want to try out an approach
    where we are allowed to create a CwlView only via
    a special constructor, and subsequently, we can
    only update it by changing the string and (if
    a parent view) loading from disk.

--- benten/editing/cwlview.py
+++ benten/editing/cwlview.py
@@ -12,16 +12,25 @@ A. The child view is set to none, which indicates to the editor to close that ta
 Q. What happens if we clear out all the text from a child?
 A. It is replaced with an empty dict, list or string depending on what type the child was
 
+Q. What happens if we have an expression view and in the parent we change that to a scalar (str, int)
+A. The child view is set to none, which indicates to the editor to close that tab
+
+Q. What happens if we are editing in one view, and an external edit (that is reloaded) has a YAML
+   error?
+A. We refuse to reload and warn the user that an external edit would result in a YAML error
+   and that error should be fixed, either by saving this workign document, or fixing the document
+   in the original external editor.
 """
-from typing import Tuple
+from typing import Tuple, Union
 import pathlib
 import os
 from enum import IntEnum, auto
 
-from .lineloader import parse_yaml_with_line_info, DocumentError, Ydict
+from ..implementationerror import ImplementationError
+from .lineloader import parse_yaml_with_line_info, DocumentError, lookup, Ydict, Ylist
 
 
-class ViewType:
+class ViewType(IntEnum):
     UnknownTool = auto()
     CommandLineTool = auto()
     ExpressionTool = auto()
@@ -30,10 +39,18 @@ class ViewType:
     Doc = auto()
 
 
+def is_expression(value):
+    return value.lstrip().startswith("$(") or value.lstrip().startswith("${")
+
+
+def is_doc_string(key):
+    return key in ["doc", "contents"]
+
+
 class CwlView:
     def __init__(self,
                  raw_cwl: str,
-                 path: pathlib.Path=None, inline_path: Tuple[(str, int), ...]=None,
+                 path: pathlib.Path=None, inline_path: Tuple[Union[str, int], ...]=None,
                  view_type: ViewType=None,
                  parent_view: 'CwlView'=None,
                  previous_view: 'CwlView'=None):
@@ -56,6 +73,7 @@ class CwlView:
             self.path = path
             self.inline_path = inline_path
             self.view_type = view_type
+            if self.view_type is None: raise RuntimeError("View type must be set.")
             self.last_known_good_dict = None
 
         self.set_raw_cwl(raw_cwl, saved=True)
@@ -70,15 +88,30 @@ class CwlView:
     def is_invalid(self):
         return self.yaml_error is not None
 
+    def is_deleted(self):
+        return self.raw_cwl is None
+
     # I want to be conscious and in control of when this (semi)expensive computation is being done
     # But I'd don't want to accidentally do it twice
     def compute_cwl_dict(self):
-        if self.cwl_dict is None:
-            try:
-                self.cwl_dict = parse_yaml_with_line_info(self.raw_cwl, convert_to_lam=True) or {}
-                self.last_known_good_dict = self.cwl_dict
-            except DocumentError as e:
-                self.yaml_error = e
+        if self.cwl_dict is not None:
+            return
+
+        try:
+            self.cwl_dict = parse_yaml_with_line_info(self.raw_cwl, convert_to_lam=True) or {}
+        except DocumentError as e:
+            self.yaml_error = e
+            return
+
+        self.last_known_good_dict = self.cwl_dict
+        if self.view_type in [ViewType.Expression, ViewType.Doc]:
+            return
+
+        self.view_type = {
+            "CommandLineTool": ViewType.CommandLineTool,
+            "ExpressionTool": ViewType.ExpressionTool,
+            "Workflow": ViewType.Workflow
+        }.get(self.cwl_dict.get("class"), ViewType.UnknownTool)
 
     def type(self):
         if self.view_type == ViewType.Process:
@@ -87,6 +120,7 @@ class CwlView:
             return
 
     def get_rel_path(self, sub_path: pathlib.Path):
+        """Relative path to put for linked sub processes"""
         return os.path.relpath(sub_path.absolute(), self.path.absolute().parent)
 
     def status(self):
@@ -126,11 +160,38 @@ class CwlView:
         if self.parent_view is None:
             self.compute_cwl_dict()
             for child_view in self.child_views:
-                child_view.set_raw_cwl(raw_cwl=self.get_view_for(child_view))
+                child_view.set_raw_cwl(raw_cwl=self.get_raw_cwl_for(child_view))
         else:
             self.parent_view.propagate_edits(self)
 
-    def get_view_for(self, child_view: 'CwlView'):
+    def get_raw_cwl_for(self, child_view: 'CwlView'):
         if self.parent_view is not None:
-            return self.parent_view.get_view_for(child_view)
+            return self.parent_view.get_raw_cwl_for(child_view)
+
+        try:
+            value = lookup(self.cwl_dict, child_view.inline_path)
+            key = child_view.inline_path[-1]
+        except (KeyError, IndexError) as e:
+            # We assume here that we never make an error in the path
+            # and a missing element has merely been deleted in a parent edit
+            return None
+
+        if not isinstance(value, (Ylist, Ydict)):
+            # This has to be a doc or an expression, to start with
+            if child_view.view_type not in [ViewType.Expression, ViewType.Doc]:
+                raise ImplementationError("This view has an error.")
+
+            # This has to be either an Expression or a doc field
+
+
+
+    def open_new_view_at(self, line, col, originating_child: 'CwlView'=None):
+        if self.parent_view is not None:
+            self.parent_view.open_new_view_at(line, col, originating_child)
+
+
+
+
+    def propagate_edits(self, child_view: 'CwlView'):
+        pass
 
