486c8b9f46a3cccd415b870527447857662cf8ac
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2019-03-09 00:00:31 -0500

    __repr__ good for debugging

--- benten/editing/edit.py
+++ benten/editing/edit.py
@@ -5,7 +5,13 @@ class EditMark:
     def __init__(self, line, column):
         self.line, self.column = line, column
 
+    def __repr__(self):  # Helpful for debugging
+        return "({}, {})".format(self.line, self.column)
+
 
 class Edit:
     def __init__(self, start, end, text):
         self.start, self.end, self.text = start, end, text
+
+    def __repr__(self):  # Helpful for debugging
+        return "{} - {}: {}".format(self.start, self.end, self.text[:10])
