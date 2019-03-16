b372692170db6cf50b72ffe099b89bc2f4ef858b
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2019-03-16 13:02:19 -0400

    This code should be added in production only
    This insulates us against errors, but we should not
    have it in dev since it tells us what we need to
    harden against.

--- benten/models/createmodel.py
+++ benten/models/createmodel.py
@@ -4,14 +4,24 @@ from .unk import Unk
 from .commandlinetool import CommandLineTool
 from .workflow import Workflow
 
+import logging
+logger = logging.getLogger(__name__)
+
 
 def infer_type(yaml_doc: YamlDoc):
-    return yaml_doc.yaml.get("class", "unknown")
+    return (yaml_doc.yaml or {}).get("class", "unknown")
 
 
 def create_model(yaml_doc: YamlDoc):
-    return {
-        "unknown": Unk,
-        "CommandLineTool": CommandLineTool,
-        "Workflow": Workflow
-    }.get(infer_type(yaml_doc), Unk)(yaml_doc)
+    try:
+        return {
+            "unknown": Unk,
+            "CommandLineTool": CommandLineTool,
+            "Workflow": Workflow
+        }.get(infer_type(yaml_doc), Unk)(yaml_doc)
+    except Exception as e:
+        raise e
+        # # As a last resort, we write this out. Means we haven't hardened our constructors enough
+        # # Should only do this in production - this removes info useful for debugging
+        # logger.error(str(e))
+        # return Unk(yaml_doc)
