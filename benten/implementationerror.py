14910f35498c78a033550fbd3db22c4ccc9484ad
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2019-03-20 09:15:18 -0400

    Add link to report issues

--- benten/implementationerror.py
+++ benten/implementationerror.py
@@ -1,3 +1,4 @@
 class ImplementationError(RuntimeError):
     def __init__(self, txt):
-        super().__init__(txt + " Please file an issue with full stack trace")
+        super().__init__(txt + "\nPlease file an issue with full stack trace at "
+                               "https://github.com/rabix/benten/issues")
