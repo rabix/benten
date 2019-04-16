4f26353330d1e83b3f70c01db6362e6220ae247d
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2019-04-16 15:33:19 -0400

    Cleanup: remove deprecated code

--- benten/implementationerror.py
+++
@@ -1,4 +0,0 @@
-class ImplementationError(RuntimeError):
-    def __init__(self, txt):
-        super().__init__(txt + "\nPlease file an issue with full stack trace at "
-                               "https://github.com/rabix/benten/issues")
