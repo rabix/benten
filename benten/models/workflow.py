1955bd8eda672a16a6e1b201630448eec62cfeb8
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2019-04-16 12:09:09 -0400

    Implement required sections in Workflow

--- benten/models/workflow.py
+++ benten/models/workflow.py
@@ -2,4 +2,8 @@ from .process import Process
 
 
 class Workflow(Process):
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        required_sections = ["cwlVersion", "class", "inputs", "outputs"]
+        self.parse_sections(required_sections)
+
