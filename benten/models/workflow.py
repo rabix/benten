ba107fe57c3acd3d8f4101de7ed7154d45676bf3
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2019-04-18 23:57:09 -0400

    Implement Workflow step navigation

--- benten/models/workflow.py
+++ benten/models/workflow.py
@@ -1,4 +1,5 @@
 from .process import Process
+from ..langserver.lspobjects import DocumentSymbol, Range, Position, SymbolKind
 
 
 class Workflow(Process):
@@ -17,3 +18,28 @@ class Workflow(Process):
             "hints": False
         }
         self.parse_sections(fields)
+
+    def symbols(self):
+        symb = super().symbols()
+        steps = next((sy for sy in symb if sy.detail == "steps"), None)
+        if steps is None:
+            return symb
+
+        steps.children = [
+            DocumentSymbol(
+                name=k,
+                detail="step",
+                kind=SymbolKind.Field,
+                _range=Range(
+                    start=Position(v.start.line, v.start.column),
+                    end=Position(v.end.line, v.end.column)
+                ),
+                selection_range=Range(
+                    start=Position(v.start.line, v.start.column),
+                    end=Position(v.end.line, v.end.column)
+                )
+            )
+            for k, v in self.ydict["steps"].items()
+        ]
+
+        return symb
