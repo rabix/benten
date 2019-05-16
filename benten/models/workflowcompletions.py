a56a3f5c5ab12a18a92721893b458957905af926
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2019-05-16 11:28:58 -0400

    Implement path picker for linked files!

--- benten/models/workflowcompletions.py
+++ benten/models/workflowcompletions.py
@@ -1,6 +1,11 @@
+import os
+
 from ..langserver.lspobjects import (
-    DocumentSymbol, Range, Position, SymbolKind, CompletionList, Location)
+    DocumentSymbol, Range, Position, SymbolKind, CompletionItem, CompletionList, Location)
 from .process import Process, resolve_file_path
+
+import logging
+logger = logging.getLogger(__name__)
 
 
 class WorkflowCompletions(Process):
@@ -9,24 +14,56 @@ class WorkflowCompletions(Process):
         p = self._compute_path(position=position)
         return {
             "steps": self._step_completion,
+            "inside step": self._inside_step_completion,
             "step_inputs": self._step_input_completion,
             None: self._no_completion
-        }.get(self._get_completion_type(p))(position, snippets)
+        }.get(self._get_completion_type(p))(p, position, snippets)
 
     def _get_completion_type(self, p):
         if len(p) and p[-1] == "steps":
             return "steps"
 
+        if len(p) and p[-2] == "steps":
+            return "inside step"
+
         if len(p) >= 4 and p[-4] == "steps" and p[-3] == "in":
             return "step_inputs"
 
-    def _no_completion(self, position: Position, snippets: dict):
+    def _prefix(self, position: Position):
+        pref = self.lines[position.line].split(":", 1)
+        if len(pref) == 2:
+            return pref[1].strip()
+        else:
+            return pref[0].strip()
+
+    def _no_completion(self, path, position: Position, snippets: dict):
         return None
 
-    def _step_completion(self, position: Position, snippets: dict):
+    def _step_completion(self, path, position: Position, snippets: dict):
         return super()._quick_completions(
             position=position, snippets=snippets,
             snippet_keys=["WFStep"])
 
-    def _step_input_completion(self, position: Position, snippets: dict):
+    def _inside_step_completion(self, path, position: Position, snippets: dict):
+
+        # Linked document file picker
+        if self.lines[position.line].lstrip().startswith("run"):
+            return [
+                CompletionItem(label=file_path)
+                for file_path in self._file_picker(self._prefix(position))
+            ]
+
+    def _file_picker(self, prefix):
+        path = resolve_file_path(self.doc_uri, prefix)
+        if not path.is_dir():
+            path = path.parent
+
+        # This is a workaround to the issue of having a dangling "." in front of the path
+        pre = os.sep if prefix == "." else ""
+
+        return (pre + str(p.relative_to(path))
+                for p in path.iterdir()
+                if p.is_dir() or str(p).endswith(".cwl"))
+
+    def _step_input_completion(self, path, position: Position, snippets: dict):
         return None
