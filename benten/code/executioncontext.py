a0487b42a6fb4fa08dd9bba3e097c1cfb3392d50
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2019-09-11 11:45:09 -0400

    Implement code to generate test inputs
    Closes #49

--- benten/code/executioncontext.py
+++ benten/code/executioncontext.py
@@ -19,9 +19,9 @@ existing test data.
 
 import pathlib
 import subprocess
-import tempfile
+import random
 
-from ..cwl.lib import un_mangle_uri
+from ..cwl.lib import un_mangle_uri, list_as_map
 
 from ruamel.yaml import YAML
 from ruamel.yaml.parser import ParserError
@@ -34,26 +34,6 @@ logger = logging.getLogger(__name__)
 
 
 job_inputs_ext = ".benten.test.job.yml"
-
-
-# def job_inputs_fname(uri):
-#     return pathlib.Path(uri + job_inputs_ext)
-
-
-# def get_job_inputs(uri):
-#     jinp = job_inputs_fname(uri)
-#
-#     _fd, _name = tempfile.mkstemp(suffix=".benten.job.yml")
-#
-#     auto_set_inputs = subprocess.run(f"cwltool --make-template {_name}", capture_output=True)
-#     user_set_inputs = {}
-#
-#     if jinp.exists():
-#         user_set_inputs = fast_load.load(jinp.open().read())
-#
-#     merge(user_set_inputs, auto_set_inputs)
-#
-#     return user_set_inputs
 
 
 def merge(user, auto):
@@ -90,43 +70,111 @@ class ExecutionContext:
         }
         self.expression_lib = []
 
-    def set_job_inputs(self, uri):
-        jinp = un_mangle_uri(uri).with_suffix(job_inputs_ext)
-        _fd, _name = tempfile.mkstemp(suffix=".benten.job.yml")
-
-        ret = subprocess.run(f"cwltool --make-template {_name}",
-                             shell=True, capture_output=True)
-
-        auto_set_inputs = {}
-        if ret.returncode:
-            self.context_error = f"Unable to create input template: {ret.stderr}"
-            logger.error(self.context_error)
-        else:
-            auto_set_inputs = fast_load.load(_fd.read())
+    def set_job_inputs(self, uri: str, cwl: dict, user_types: dict):
+        cwl_uri = un_mangle_uri(uri)
+        ex_job_file = cwl_uri.with_suffix(job_inputs_ext)
+        _inputs = cwl.get("inputs")
+        if not isinstance(_inputs, (list, dict)):
+            _inputs = {}
+        auto_set_inputs = {
+            k: example_value(k, _type_v, user_types)
+            for k, _type_v in list_as_map(_inputs, key_field="id", problems=[]).items()
+        }
 
         user_set_inputs = {}
-        if jinp.exists():
+        if ex_job_file.exists():
             try:
-                user_set_inputs = fast_load.load(jinp.open().read() or "")
+                user_set_inputs = fast_load.load(ex_job_file.open().read() or "")
                 logger.debug(user_set_inputs)
             except (ParserError, ScannerError) as e:
-                logger.error(f"Parsing error loading test input file {jinp}")
+                logger.error(f"Parsing error loading test input file {ex_job_file}")
                 user_set_inputs = {}
 
-        # merge(user_set_inputs, auto_set_inputs)
+        merge(user_set_inputs, auto_set_inputs)
         self.job_inputs = user_set_inputs
-        fast_load.dump(user_set_inputs, jinp)
+        logger.debug(self.job_inputs)
 
     def set_expression_lib(self, expression_lib: list=None):
         self.expression_lib = expression_lib
 
 
-# class ExpressionLib:
-#
-#     def __init__(self, expression_lib: list=None):
-#         self.expression_lib = expression_lib or []
-#
-#     def eval(self, js_exp: str, input_object: dict):
-#         return dukpy.evaljs(self.expression_lib + [js_exp], **input_object)
+def basic_example_value(name, _type):
+    if _type == 'null':
+        return 'null'
+    elif _type == 'Any':
+        return 'Any'
+    elif _type == 'boolean':
+        return random.randint(0, 1) > 0
+    elif _type == 'int' or _type == 'long':
+        return random.randint(-1000, 1000)
+    elif _type == 'float' or _type == 'double':
+        return random.random() * 100 - 50
+    elif _type == 'string':
+        return name
+    elif _type == 'File':
+        return {
+            'class': 'File',
+            'path': f'/path/to/{name}.ext',
+            'location': f'/location/of/{name}.ext',
+            'basename': f'{name}.ext',
+            'dirname': '/path/to/',
+            'nameroot': name,
+            'nameext': '.ext',
+            'checksum': "sha1$deadbeef",
+            'size': random.randint(0, 4096),
+            'format': 'someformat',
+            'contents': 'To be, or not to be. That is the question.'
+        }
+    elif _type == 'Directory':
+        return {
+            'class': 'Directory',
+            'path': f'/path/to/{name}'
+        }
 
 
+def enum_example_value(symbols):
+    return symbols[random.randint(0, len(symbols) - 1)]
+
+
+def record_example_value(name, _type, user_types):
+    return {
+        k: example_value(name, _type_v, user_types)
+        for k, _type_v in list_as_map(_type.get("fields"), key_field="name", problems=[]).items()
+    }
+
+
+def example_value(name, cwl_type, user_types, array_of=False):
+    if array_of:
+        return [example_value(name + "/" + str(i), cwl_type, user_types) for i in range(4)]
+
+    if isinstance(cwl_type, list):
+        l = len(cwl_type)
+        return example_value(name, cwl_type[random.randint(0, l - 1)], user_types)
+
+    elif isinstance(cwl_type, dict) and "type" in cwl_type:
+        _type = cwl_type.get("type")
+        if _type == "array":
+            return example_value(name, _type.get("items"), user_types, array_of=True)
+        elif _type == "enum":
+            return enum_example_value(cwl_type.get("symbols"))
+        elif _type == "record":
+            return record_example_value(name, cwl_type, user_types)
+        else:
+            return example_value(name, _type, user_types)
+
+    elif isinstance(cwl_type, str):
+        # desugar
+        if cwl_type.endswith("?"):
+            cwl_type = cwl_type[:-1]
+
+        if cwl_type.endswith("[]"):
+            cwl_type = {
+                "type": "array",
+                "items": [cwl_type[:-2]]
+            }
+            return example_value(name, cwl_type, user_types)
+
+        if cwl_type in user_types:
+            return example_value(name, user_types.get(cwl_type), user_types)
+
+        return basic_example_value(name, cwl_type)
