130d486eccd524753858b01d6290c962524bcd1f
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2019-09-13 11:53:44 -0400

    Implement `self`
    Closes #56

--- benten/cwl/expressiontype.py
+++ benten/cwl/expressiontype.py
@@ -9,6 +9,7 @@ from .basetype import (CWLBaseType, MapSubjectPredicate, TypeCheck, Match,
                        Intelligence, IntelligenceContext)
 from ..langserver.lspobjects import Range, Hover, Location
 from ..code.intelligence import LookupNode
+from ..code.executioncontext import basic_example_value
 
 import logging
 logger = logging.getLogger(__name__)
@@ -49,18 +50,20 @@ class CWLExpression(CWLBaseType):
     def __init__(self, expression: str, exp_type: ExpressionType, bracketing_terms=None):
         super().__init__("Expression")
         self.bracketing_terms = bracketing_terms or ["", ""]
-        self.self = ()  # the path to the 'self' variable as described in CWL specs
+        self.self_path = ()  # the path to the 'self' variable as described in CWL specs
         if exp_type is ExpressionType.ParameterReference:
             self.expression = \
                 f"""
 var runtime = dukpy['runtime'];
 var inputs = dukpy['inputs'];
+var self = dukpy['cwl_self'];
 {expression}"""
         else:
             self.expression = \
                 f"""
 var runtime = dukpy['runtime'];
 var inputs = dukpy['inputs'];
+var self = dukpy['cwl_self'];
 function benten_eval_func() {{
     {expression} 
 }}; 
@@ -82,18 +85,47 @@ benten_eval_func()"""
 
         self.execution_context = code_intel.execution_context
 
+        # Deal with filling out self
+        if intel_context.path[-1] in ["secondaryFiles"]:
+            self.self_path = ('inputs', intel_context.path[-2])
+
+        if intel_context.path[-1] in ["position", "valueFrom"]:
+            if intel_context.path[-2] == "inputBinding":
+                self.self_path = ('inputs', intel_context.path[-3])
+
+        if intel_context.path[-1] in ["outputEval"]:
+            self.self_path = ('outputs', intel_context.path[-3])
+            logger.warning("Benten can't properly simulate globbed files yet")
+            pass
+            # todo: Check for the glob and then simulate globbed files
+
         ln = LookupNode(loc=value_range)
         ln.intelligence_node = self
         code_intel.add_lookup_node(ln)
 
     def hover(self):
         job_inputs = self.execution_context.job_inputs
+        cwl_self = None
+
+        if self.self_path:
+            if self.self_path[0] == 'inputs':
+                cwl_self = job_inputs[self.self_path[1]]
+            if self.self_path[0] == 'outputs':
+                cwl_self = [basic_example_value(self.self_path[1] + "/" + str(i), "File")
+                            for i in range(4)]
+
         if job_inputs:
             try:
                 res = dukpy.evaljs(self.execution_context.expression_lib + [self.expression],
                                    runtime=self.execution_context.runtime,
-                                   inputs=job_inputs)
-                res = self.bracketing_terms[0] + str(res) + self.bracketing_terms[1]
+                                   inputs=job_inputs,
+                                   cwl_self=cwl_self)
+
+                if res is None and self.exp_type == ExpressionType.JSExpression:
+                    res = "Got a 'null' result. Do you have a `return` for your JS expression?"
+                else:
+                    res = self.bracketing_terms[0] + str(res) + self.bracketing_terms[1]
+
             except dukpy.JSRuntimeError as e:
                 res = str(e).splitlines()[0]
                 logger.error(res)
