e29348a2d4e915b316baac62b13492fe797fd532
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2019-09-20 15:07:49 -0400

    Removing SB platform related code
    Operations dealing with pushing and running the code
    on SBPLA are best done with a separate library.

--- benten/sbg/sbgcwldoc.py
+++
@@ -1,9 +0,0 @@
-#  Copyright (c) 2019 Seven Bridges. See LICENSE
-
-from .versionmixin import VersionMixin
-from ..editing.cwlprocess import CwlProcess
-
-
-class SBGCwlDoc(VersionMixin, CwlProcess):
-    def __init__(self, *args, **kwargs):
-        super(SBGCwlDoc, self).__init__(*args, **kwargs)
