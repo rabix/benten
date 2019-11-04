c26a430e61249bcacbc503e47233ee84cc3037b2
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2019-11-04 12:48:49 -0500

    Remove meaningless CLI option

--- benten/__main__.py
+++ benten/__main__.py
@@ -56,7 +56,6 @@ def main():
     parser.add_argument(
         "--addr", default=4389, help="server listen (tcp)", type=int)
     parser.add_argument("--debug", action="store_true")
-    parser.add_argument("--python_path")
 
     args = parser.parse_args()
 
