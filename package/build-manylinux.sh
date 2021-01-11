90b8d2fd4dd4d86a71603b6310b9f9536a1d025d
Peter Amstutz <peter.amstutz@curii.com>
2021-01-11 11:18:23 -0500

    Add script to build using 'manylinux'
    Use a slightly modified fork of the 'manylinux' Docker image to achive
    binary support over the widest possible range of Linux distributions.

---
+++ package/build-manylinux.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [[ $1 != "--inside" ]]; then
+    exec docker run --rm -v$PWD:/benten -w/benten quay.io/tetron/manylinux2010_x86_64:fc5d4309b9cff6e58a2d0e5d9d4eba0c797c655b /benten/package/build-manylinux.sh --inside
+fi
+
+set -xe
+
+export LD_LIBRARY_PATH=/usr/local/lib:/opt/python/cp38-cp38/lib
+
+rm -rf bvenv
+/opt/python/cp38-cp38/bin/python -m venv bvenv
+. bvenv/bin/activate
+
+pip install .
+pip install pyinstaller==4.0
+
+cd package
+rm -rf dist
+pyinstaller --add-binary '/usr/local/lib/libcrypt.so.2:.' benten-ls.spec
+python tgz.py
